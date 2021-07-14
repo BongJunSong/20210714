74c5beb1a1848d04f3b87b7ef23e84714983d286
BongJunSong <chaosts0981@gmail.com>
2021-07-14 12:55:22 +0900

    20210714-Typescript

---
+++ class_note/2_function.ts
@@ -0,0 +1,16 @@
+//2021/07/14 practice
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+add(10, 20);
+
+
+// TS optional parameter 
+function log(a: string, b?: string) {
+
+}
+
+log("test");
+log("test1", "test2");
